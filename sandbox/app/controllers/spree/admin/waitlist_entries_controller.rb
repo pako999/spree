ce1af188f661735773085ff66c1c52ff0bb19118
Patrik <info@surf-store.com>
2026-02-21 11:40:16 +0100

    Fix missing table registry and waitlist pagination crash

--- sandbox/app/controllers/spree/admin/waitlist_entries_controller.rb
+++ sandbox/app/controllers/spree/admin/waitlist_entries_controller.rb
@@ -1,18 +1,14 @@
 module Spree
   module Admin
     class WaitlistEntriesController < Spree::Admin::ResourceController
-      def index
-        @entries = collection
-      end
-
       private
 
-      def collection
-        return @collection if @collection.present?
-        
-        params[:q] ||= {}
-        @search = WaitlistEntry.ransack(params[:q])
-        @collection = @search.result.includes(variant: :product).order(created_at: :desc).page(params[:page]).per(params[:per_page] || Spree::Config[:admin_products_per_page])
+      def collection_includes
+        [{ variant: :product }]
+      end
+
+      def collection_default_sort
+        { created_at: :desc }
       end
 
       def model_class
