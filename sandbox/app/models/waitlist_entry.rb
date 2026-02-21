bf60ab18c9747ef7eb2b5a296ed4a5764ce1508e
Patrik <info@surf-store.com>
2026-02-21 08:25:25 +0100

    Add missing WaitlistEntry model to fix 500 error

---
+++ sandbox/app/models/waitlist_entry.rb
@@ -0,0 +1,10 @@
+class WaitlistEntry < ApplicationRecord
+  belongs_to :variant, class_name: 'Spree::Variant'
+
+  # A user shouldn't be able to sign up for the exact same variant multiple times 
+  # unless their previous pending request has been notified (fulfilled).
+  validates :email, uniqueness: { scope: :variant_id, message: "is already on the waitlist for this item" }, conditions: -> { pending }
+
+  # Scopes
+  scope :pending, -> { where(notified_at: nil) }
+end
