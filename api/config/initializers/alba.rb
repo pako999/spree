ba72d0169736bda1f9897cb81fca2bcc1c22a972
pako999 <125508378+pako999@users.noreply.github.com>
2026-02-22 12:59:49 +0100

    Fix Alba Typelizer crash with global iso8601 mapping patch

--- api/config/initializers/alba.rb
+++ api/config/initializers/alba.rb
@@ -3,3 +3,21 @@ Alba.inflector = :active_support
 
 # Custom types
 Alba.register_type :iso8601, converter: ->(time) { time&.iso8601(3) }, auto_convert: true
+
+begin
+  require 'typelizer/serializer_plugins/alba'
+  module Typelizer
+    module SerializerPlugins
+      module Alba
+        unless method_defined?(:original_ts_mapper)
+          alias_method :original_ts_mapper, :ts_mapper
+          def ts_mapper
+            original_ts_mapper.merge('iso8601' => { type: 'string' })
+          end
+        end
+      end
+    end
+  end
+rescue LoadError
+  # typelizer not in bundle, safely ignore
+end
